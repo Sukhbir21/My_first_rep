47292e9c4a20743c94c2d5bc9dd729909c671a25
Sukhbir21 <81358953+Sukhbir21@users.noreply.github.com>
2021-03-25 16:25:54 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,13 +2,16 @@ echo "how many files are there in directory"
 read option
 
 nof=$ ls | wc -l
-if [[ $option -eq $nof ]]
-then
-  echo "exactly same number"
-else if [[ $option -gt $nof ]]
-then 
-  echo "too high"
-else
-  echo "to low"
+while [[ $option -gt 0 ]]
+do
+  if [[ $option -eq $nof ]]
+  then
+    echo "exactly same number"
+  else if [[ $option -gt $nof ]]
+  then 
+    echo "too high"
+  else
+    echo "to low"
+done
 
 
